c21a87249af5837e06722e08e9f9a1c954a6f4be
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2024-08-07 22:29:35 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,9 @@
+//HackerRank code for java- 1
+//printing the sentences
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+       System.out.println("Hello, Java."); 
+    }
+}
