7aa1bd0983bb77dc5feef6a0a1261da0a2ed6e1c
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2024-08-07 22:36:59 +0530

    Add files via upload

---
+++ InputbyUser.java
@@ -0,0 +1,16 @@
+//HackerRank code for java- 2
+//In this we are taking the input from user using Scanner class
+import java.util.Scanner;
+
+public class InputbyUser {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
