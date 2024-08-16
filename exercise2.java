2c6fc85404d0f4600f8cefeb53a7111d82fccb78
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2024-08-16 10:14:47 +0530

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class exercise2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter  1st choice: ");
+        String per1 = sc.next();
+        System.out.println("enter 2nd person choice");
+        String per2 = sc.next();
+        System.out.println("enter 3rd person choice");
+        String per3 = sc.next();
+        if()
+    }
+}
